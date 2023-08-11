bd7214658755e95d2c9c4b4f3d18ac373be7bc85
Sagor saha <sahasagor650@gmail.com>
2023-08-12 00:30:36 +0600

    hi

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'media',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+}
+
